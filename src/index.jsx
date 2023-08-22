6a2a6bebaf76d7d00aa605d8ab9263ad0d4156dc
waqas rafiq <wm66179@gmail.com>
2023-08-23 02:09:19 +0500

    Final Resume

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import Profile from "./profile.jpg";
-import 'bootstrap/dist/css/bootstrap.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 
 function Resume() {
@@ -11,18 +11,15 @@ function Resume() {
         <div id="page-top">
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
             <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                <span className="d-block d-lg-none">Clarence Taylor</span>
+                <span className="d-block d-lg-none">Muhammad Waqas</span>
                 <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={Profile} alt="..." /></span>
             </a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav">
                     <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
                     <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
                     <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#education">Education</a></li>
                     <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
                 </ul>
             </div>
         </nav>
@@ -30,20 +27,14 @@ function Resume() {
             <section className="resume-section" id="about">
                 <div className="resume-section-content">
                     <h1 className="mb-0">
-                        Clarence
-                        <span className="text-primary">Taylor</span>
+                        Muhammad
+                        <span className="text-primary">Waqas</span>
                     </h1>
                     <div className="subheading mb-5">
-                        3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
-                        <a href="mailto:name@email.com">name@email.com</a>
+                        · 03102604311 ·
+                        <a href="mailto:wm66179@gmail.com">waqas@gmail.com</a>
                     </div>
-                    <p className="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
-                    <div className="social-icons">
-                        <a className="social-icon" href="#!"><i className="fab fa-linkedin-in"></i></a>
-                        <a className="social-icon" href="#!"><i className="fab fa-github"></i></a>
-                        <a className="social-icon" href="#!"><i className="fab fa-twitter"></i></a>
-                        <a className="social-icon" href="#!"><i className="fab fa-facebook-f"></i></a>
-                    </div>
+                    <p className="lead mb-5">MERN Stack Developer with a Passion for Creating Beautiful Web & Mobile Apps.</p>
                 </div>
             </section>
             <hr className="m-0" />
@@ -52,35 +43,11 @@ function Resume() {
                     <h2 className="mb-5">Experience</h2>
                     <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div className="flex-grow-1">
-                            <h3 className="mb-0">Senior Web Developer</h3>
-                            <div className="subheading mb-3">Intelitec Solutions</div>
-                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+                            <h3 className="mb-0">Preview Officer</h3>
+                            <div className="subheading mb-3">Madani Channel</div>
+                            <p>I meticulously review videos and news content to ensure their quality and accuracy before onair, contributing to the seamless delivery of reliable information to viewers.</p>
                         </div>
-                        <div className="flex-shrink-0"><span className="text-primary">March 2013 - Present</span></div>
-                    </div>
-                    <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
-                        <div className="flex-grow-1">
-                            <h3 className="mb-0">Web Developer</h3>
-                            <div className="subheading mb-3">Intelitec Solutions</div>
-                            <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
-                        </div>
-                        <div className="flex-shrink-0"><span className="text-primary">December 2011 - March 2013</span></div>
-                    </div>
-                    <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
-                        <div className="flex-grow-1">
-                            <h3 className="mb-0">Junior Web Designer</h3>
-                            <div className="subheading mb-3">Shout! Media Productions</div>
-                            <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
-                        </div>
-                        <div className="flex-shrink-0"><span className="text-primary">July 2010 - December 2011</span></div>
-                    </div>
-                    <div className="d-flex flex-column flex-md-row justify-content-between">
-                        <div className="flex-grow-1">
-                            <h3 className="mb-0">Web Design Intern</h3>
-                            <div className="subheading mb-3">Shout! Media Productions</div>
-                            <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
-                        </div>
-                        <div className="flex-shrink-0"><span className="text-primary">September 2008 - June 2010</span></div>
+                        <div className="flex-shrink-0"><span className="text-primary">November 2020 - Present</span></div>
                     </div>
                 </div>
             </section>
@@ -90,20 +57,15 @@ function Resume() {
                     <h2 className="mb-5">Education</h2>
                     <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div className="flex-grow-1">
-                            <h3 className="mb-0">University of Colorado Boulder</h3>
-                            <div className="subheading mb-3">Bachelor of Science</div>
-                            <div>Computer Science - Web Development Track</div>
-                            <p>GPA: 3.23</p>
+                            <h3 className="mb-0">Govt. Superior Commerce College</h3>
+                            <div className="subheading mb-3">Inter in Commerce</div>
                         </div>
-                        <div className="flex-shrink-0"><span className="text-primary">August 2006 - May 2010</span></div>
                     </div>
                     <div className="d-flex flex-column flex-md-row justify-content-between">
                         <div className="flex-grow-1">
-                            <h3 className="mb-0">James Buchanan High School</h3>
-                            <div className="subheading mb-3">Technology Magnet Program</div>
-                            <p>GPA: 3.56</p>
+                            <h3 className="mb-0">Govt. Boys Secondary School</h3>
+                            <div className="subheading mb-3">Metric in Science</div>
                         </div>
-                        <div className="flex-shrink-0"><span className="text-primary">August 2002 - May 2006</span></div>
                     </div>
                 </div>
             </section>
@@ -112,37 +74,42 @@ function Resume() {
                 <div className="resume-section-content">
                     <h2 className="mb-5">Skills</h2>
                     <div className="subheading mb-3">Programming Languages & Tools</div>
-                    <ul className="list-inline dev-icons">
-                        <li className="list-inline-item"><i className="fab fa-html5"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-css3-alt"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-js-square"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-angular"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-react"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-node-js"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-sass"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-less"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-wordpress"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-gulp"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-grunt"></i></li>
-                        <li className="list-inline-item"><i className="fab fa-npm"></i></li>
-                    </ul>
-                    <div className="subheading mb-3">Workflow</div>
                     <ul className="fa-ul mb-0">
                         <li>
                             <span className="fa-li"><i className="fas fa-check"></i></span>
+                            HTML5
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
+                            CSS3
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
+                            Bootstrap
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
+                            Firebase
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
+                            Nodejs
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
+                            Express
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
+                            Mongodb
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
+                            Reactjs
+                        </li>
+                        <li>
+                            <span className="fa-li"><i className="fas fa-check"></i></span>
                             Mobile-First, Responsive Design
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Cross Browser Testing & Debugging
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Cross Functional Teams
-                        </li>
-                        <li>
-                            <span className="fa-li"><i className="fas fa-check"></i></span>
-                            Agile Development & Scrum
                         </li>
                     </ul>
                 </div>
